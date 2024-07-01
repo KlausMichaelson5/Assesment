70306f755bbf50e0672bdc02c36dd5e3f716b7f2
KlausMichaelson5 <95432072+KlausMichaelson5@users.noreply.github.com>
2024-07-01 15:36:45 +0530

    Add files via upload

---
+++ CodeGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assement_Q1
+{
+    internal class CodeGenerater
+    {
+        private static string sentence = "The quick and brown fox jumps over the Lazy Dog";
+
+        private static Dictionary<char, string> letterCodes = GenerateCode(sentence);
+
+        private static Dictionary<char, string> GenerateCode(string sentence)
+        {
+            Dictionary<char, string> letterCodes = new Dictionary<char, string>();
+            string[] wordsList = sentence.Split(' ');
+            for (int wordIndex = 0; wordIndex < wordsList.Length; wordIndex++)
+            {
+                string word = wordsList[wordIndex];
+                for (int letterIndex = 0; letterIndex < word.Length; letterIndex++)
+                {
+                    if (!letterCodes.ContainsKey(word[letterIndex]))
+                    {
+                        string code = $"{wordIndex}{letterIndex}";
+                        letterCodes[word[letterIndex]] = code;
+                    }
+                }
+            }
+            return letterCodes;
+        }
+
+        public static Dictionary<char, string> GetLetterCodes() => letterCodes;
+
+    }
+}
---
+++ Encoder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assement_Q1
+{
+    internal class Encoder
+    {
+        public static string Encode(string message)
+        {
+            if (message == null) throw new NullReferenceException("Please provide a string to encode.");
+
+            if (message == "") throw new ArgumentException("Please provide a non empty string to encode.");
+
+            Dictionary<char, string> letterCodes = CodeGenerater.GetLetterCodes();
+
+            string result = "";
+            for (int messageIndex = 0; messageIndex < message.Length; messageIndex++)
+            {
+                if (message[messageIndex] == ' ')
+                {
+                    result += '-';
+                    continue;
+                }
+                result += GetCodeForLetter(message[messageIndex], letterCodes);
+                if (messageIndex != message.Length - 1 && message[messageIndex + 1] != ' ')
+                {
+                    result += ',';
+                }
+            }
+
+            return result;
+        }
+
+        private static string GetCodeForLetter(char letter, Dictionary<char, string> letterCodes)
+        {
+            letter = char.ToLower(letter);
+            if (letterCodes.ContainsKey(letter))
+            {
+                return letterCodes[letter];
+            }
+            else
+            {
+               throw new ArgumentException("Please provide a alphabet to get its associated code.");
+            }
+        }
+    }
+
+}
